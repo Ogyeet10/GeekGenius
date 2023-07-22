051d8c192ee2b55f6c8189893b0d0dbed2c86e0c
Aidan <NO>
2023-07-22 14:37:20 -0500

    added tip jar(broken)

---
+++ GeekGenius/Helpers/MvProductsids.swift
@@ -0,0 +1,16 @@
+//
+//  MvProductsids.swift
+//  GeekGenius
+//
+//  Created by Aidan Leuenberger on 7/21/23.
+//
+
+import Foundation
+
+let myTipProductIdentifiers = [
+"TipJar",
+"com.oggroup.GeekGenius.smallTip",
+"com.oggroup.GeekGenius.helpfulTip",
+"com.oggroup.GeekGenius.bigTip",
+"com.oggroup.GeekGenius.insaneTip"
+]
